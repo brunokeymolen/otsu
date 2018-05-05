1b9aa332c94bd21cb78530655f8f47199cc17f90
Bruno Keymolen <bruno.keymolen@gmail.com>
2018-05-05 22:47:33 +0200

    added math

--- README.md
+++ README.md
@@ -3,11 +3,10 @@ Otsu
 
 Otsu thresholding in C++
 
-* WORK IN PROGRESS *
-
 
 <a href="https://en.wikipedia.org/wiki/Otsu's_method">https://en.wikipedia.org/wiki/Otsu's_method</a>
-
+<br>
+<a href="http://www.labbookpages.co.uk/software/imgProc/otsuThreshold.html">http://www.labbookpages.co.uk/software/imgProc/otsuThreshold.html</a>
 
 <img src="results/valve_original.png" width="550">
 
--- otsu.cpp
+++ otsu.cpp
@@ -71,24 +71,71 @@ unsigned char* Otsu::Threshold(const unsigned char* src, unsigned char* hist,
                                unsigned char* result) {
     memset(histogram_, 0, 255 * sizeof(int));
 
+
+    //create histogram
+    int histmax = 0;
     for (int x = 0; x < w_; x++) {
         for (int y = 0; y < h_; y++) {
             int pos = x + (y * w_);
             histogram_[src[pos]]++;
+            //if(histogram_[src[pos]] > histmax)
+            //{
+            //    histmax = histogram_[src[pos]];
+            //}
         }
     }
 
-    int max = 0;
-    for (int z = 0; z < 255; z++) {
-        if (histogram_[z] > max) {
-            max = histogram_[z];
+    for (int z=0; z<255; z++)
+    {
+        if (histogram_[z] > histmax)
+        {
+            histmax = histogram_[z];
         }
     }
+
+    double total = w_ * h_;
+    double tot = 0.0;
+    double wb = 0.0;
+    double wf = 0.0;
+    double btot = 0.0;
+    double max = 0.0;
+    int t = 0;
+   
+    for (int x=0; x<255; x++)
+    {
+        tot += (double)(histogram_[x] * x);
+    }
+    
+    for (int x=0; x<255; x++)
+    {
+        wb += histogram_[x];
+        if (wb == 0)
+        {
+            continue;
+        }
+        wf = total - wb;
+        if (wf == 0)
+        {
+            break;
+        }
+
+        btot += (double)(x * histogram_[x]);
+
+        double ub = btot / wb;
+        double uf = (tot - btot) / wf;
+
+        double variance = wb * wf * (ub - uf) * (ub - uf);
+        if (variance > max)
+        {
+            max = variance;
+            t = x;
+        }
+
+     }
 
     //Draw histogram
-    double scale = (double)(HISTOGRAM_H - 10) / (double)max;
+    double scale = (double)(HISTOGRAM_H - 10) / (double)histmax;
     double bw = (HISTOGRAM_W - 10) / 255;
-    std::cout << "max: " << max << ", scale: " << scale << std::endl;
     for (int z = 0; z < 255; z++) {
         DrawBox(hist, HISTOGRAM_W, 
                 (z * bw) + 10, HISTOGRAM_H - 5 - (int)(histogram_[z] * scale), 
@@ -96,17 +143,22 @@ unsigned char* Otsu::Threshold(const unsigned char* src, unsigned char* hist,
                 200);
     }
 
+    DrawBox(hist, HISTOGRAM_W, 
+                (t * bw) + 10, 0, 
+                (t * bw) + bw + 10, HISTOGRAM_H, 
+                250);
+ 
     
     for (int x = 0; x < w_; x++) {
         for (int y = 0; y < h_; y++) {
             int pos = x + (y * w_);
-            if (src[pos] > 120) 
+            if (src[pos] > t) 
             {
-                result[pos] = 0;
+                result[pos] = 255;
             }
             else
             {
-                result[pos] = src[pos];//255;
+                result[pos] = 0;
             }
         }
     }
